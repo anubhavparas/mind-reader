c11d8fd91a4a9168fee7a900eab6de8ee8023b40
Anubhav Paras <32901101+anubhavparas@users.noreply.github.com>
2024-10-20 00:26:26 -0700

    Update mind_reader.py

--- mind_reader.py
+++ mind_reader.py
@@ -11,16 +11,20 @@ def read_mind() -> None:
   st.header("Let's read your mind... :bulb:")
 
   submitted = False
-  num = None
-  fav_dish = None
+  num_inp = None
+  fav_dish_inp = None
   with st.form("mind_reader_form"):
     num = st.number_input("Think of a number between 1-100...", max_value=100, step=1)
     if 1 <= num <= 100:
       fav_dish = st.text_input("What's your favorite dish? :yum:")
 
     submitted = st.form_submit_button("Go")
-  if submitted and num and fav_dish:
-      action(num, fav_dish)
+    if submitted and num and fav_dish:
+      num_inp = num
+      fav_dish_inp = fav_dish
+  
+  if num_inp and fav_dish_inp:
+    action(num_inp, fav_dish_inp)
 
 def action(num: int, fav_dish: str) -> None:
   """
@@ -41,7 +45,7 @@ def action(num: int, fav_dish: str) -> None:
     st.info("Okay! I am ready now...")
     time.sleep(sleep_time)
 
-  st.info(f"You were thinking about the number: {num} and your favorite dish is: {fav_dish}... :sunglasses:")
+    st.info(f"You were thinking about the number: {num} and your favorite dish is: {fav_dish}... :sunglasses:")
 
   st.balloons()
 
