1b70afb65d819d816b269d706e7dc70193f64548
Anubhav Paras <32901101+anubhavparas@users.noreply.github.com>
2024-10-20 01:08:20 -0700

    Update mind_reader.py

--- mind_reader.py
+++ mind_reader.py
@@ -42,6 +42,12 @@ def action(num: int, fav_dish: str) -> None:
     st.info(f"You were thinking about the number {num} and your favorite dish is {fav_dish}... :sunglasses:")
 
   st.balloons()
+  with open("query_count", "r+") as file:
+    count = file.read()
+    if not count:
+      count = 0
+    else count = int(count)
+    file.write(str(count + 1))
 
 
 if __name__ == "__main__":
